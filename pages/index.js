846d5e330dd3b9cf648f5d416e10d00697401cef
BuildTools <unconfigured@null.spigotmc.org>
2022-10-17 17:48:55 +0900

    test2

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export async function getServerSideProps() {
     };
 }
 
-export default function Home({ messages, id }) {
+export default function Home({ messages }) {
     const [name, setName] = useState("名無し");
     const [text, setText] = useState("");
 
