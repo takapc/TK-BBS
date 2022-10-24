02b4f943b8a12dcb046c7bc0f6a555b5c3d0aa2b
chell_uoxou <chell_uoxou@yahoo.co.jp>
2022-10-24 20:09:11 +0900

    Update many things

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import styles from "../styles/main.module.css";
+import { toast, Toaster } from "react-hot-toast";
 import Chat from "../components/message.js";
 import Router from "next/router";
 import { useState } from "react";
+import { animateScroll as scroll } from "react-scroll";
 
 export async function getServerSideProps() {
     const chat = await fetch("https://tk-api.onrender.com/get");
@@ -20,48 +22,114 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ messages }) {
-    const [name, setName] = useState("名無し");
+    const [isSubmitting, setSubmitting] = useState(false);
+    const [name, setName] = useState("");
     const [text, setText] = useState("");
 
     const nameChange = (e) => {
         setName(() => e.target.value);
     };
+
     const textChange = (e) => {
         setText(() => e.target.value);
     };
+
     async function onSubmit() {
-        if (text == "") return;
-        const chat = await fetch("https://tk-api.onrender.com/get");
+        if (text == "") {
+            toast.error("本文が空です");
+            return;
+        }
+
+        const toastId = toast.loading("投稿中...");
+        setSubmitting(true);
+
+        const chat = await fetch("https://tk-api.onrender.com/get")
+        .catch((e)=>{
+            setSubmitting(false);
+            toast.error("投稿に失敗しました", { id: toastId })
+            throw Error(e);
+        });
+
         const data = await chat.json();
+
         let ids = [0];
         data.docs.map((value) => {
             ids.push(value.id);
         });
         ids.sort((a, b) => a - b).reverse();
+
         const postItem = {
             id: ids[0] + 1,
-            name: `${name}`,
+            name: `${name ? name : "名無し"}`,
             content: text,
             created: Date.now(),
         };
+
         const res = await fetch("https://tk-api.onrender.com/post", {
             method: "POST",
             body: JSON.stringify(postItem),
             headers: {
                 "Content-Type": "application/json",
             },
+        }).catch((e)=>{
+            toast.error("投稿に失敗しました", { id: toastId })
+            setSubmitting(false);
+            throw Error(e);
         });
-        Router.push("/", undefined, { scroll: false });
+
+        if (res.ok) {
+            toast.success("投稿しました", { id: toastId });
+            setText("")
+            Router.push("/", undefined, { scroll: false });
+            scroll.scrollToBottom();
+        } else {
+            toast.error("投稿に失敗しました", { id: toastId });
+        }
+
+        setSubmitting(false);
     }
 
     async function onReload() {
         Router.push("/", undefined, { scroll: false });
+        toast.success("更新しました");
+        scroll.scrollToBottom();
     }
+
     return (
         <div>
-            <h1 className={styles.titlePos}>
-                <span className={styles.title}>TK 匿名掲示板</span>
-            </h1>
+            <Toaster
+                position="top-center"
+                reverseOrder={false}
+            />
+            <div className={styles.fixedComponentsContainer}>
+                <h1 className={styles.titlePos}>
+                    <span className={styles.title}>TK 匿名掲示板</span>
+                </h1>
+
+                <h2 className={styles.formTitle}>新規投稿</h2>
+                <div className={styles.inputForm}>
+                    <input
+                        className={styles.inputName}
+                        value={name}
+                        placeholder="名前"
+                        onChange={nameChange}
+                    ></input>
+                    <textarea
+                        className={styles.inputContent}
+                        value={text}
+                        placeholder="本文"
+                        maxLength="1000"
+                        onChange={textChange}
+                    ></textarea>
+                    <button className={styles.sendChat} onClick={onSubmit} disabled={isSubmitting}>
+                        投稿
+                    </button>
+                    <button className={styles.sendChat} onClick={onReload}>
+                        更新
+                    </button>
+                </div>
+            </div>
+            <div className={styles.messagesContainer}>
             {messages.map((value) => {
                 return (
                     <div key={value._id}>
@@ -75,25 +143,6 @@ export default function Home({ messages }) {
                     </div>
                 );
             })}
-            <h2 className={styles.form}>入力フォーム</h2>
-            <div className={styles.inputForm}>
-                <button className={styles.sendChat} onClick={onSubmit}>
-                    投稿
-                </button>
-                <button className={styles.sendChat} onClick={onReload}>
-                    更新
-                </button>
-                <input
-                    className={styles.inputName}
-                    placeholder="名前を入力"
-                    onChange={nameChange}
-                ></input>
-                <textarea
-                    className={styles.inputContent}
-                    placeholder="テキストを入力"
-                    maxLength="1000"
-                    onChange={textChange}
-                ></textarea>
             </div>
         </div>
     );
